df36bb19378e01133898edb8728eb84bbd4443e0
bsce25033 <bsce25033@itu.edu.pk>
2026-03-29 22:11:50 +0500

    bsce25033

--- functions.cpp
+++ functions.cpp
@@ -2,11 +2,20 @@
 
 
 int sumOfSquares(int n) 
-{
+{//Base case
+    if (n <= 0)
+        return 0;
+    //Recursive case
+    return n * n + sumOfSquares(n - 1);
 }
 
 int findFactorial(int n)
 {
+    //Base case
+    if (n == 0 || n == 1)
+        return 1;
+    //Recursive case
+    return n * findFactorial(n - 1);
 }
 
 
--- main.cpp
+++ main.cpp
@@ -1,6 +1,41 @@
 #include "functions.h"
 
+int main()
+{
+    int choice;
+    do{
 
-int main() {
-   
+
+        cout<<"1. Sum of Squares"<<endl;
+        cout<<"2. Find Factorial"<<endl;
+        cout <<"3. Exit" << endl;
+        cout << "Please enter a choice: "<<endl;
+        cin >> choice;
+
+        switch(choice)
+        {
+        case 1:{
+                int n;
+                cout<<"Please enter a number: "<<endl;
+                cin>>n;
+                cout<< sumOfSquares(n)<<endl;
+                break;
+        }
+        case 2:{
+                int number;
+                cout<<"Please enter a number: "<<endl;
+                cin>>number;
+                cout<< findFactorial(number)<<endl;
+                break;
+        }
+        case 3:
+            cout<<"Exiting the program!!!LOL "<<endl;
+            break;
+
+        default:
+            cout<<"Please enter a valid choice shit"<<endl;
+        }
+    }while(choice!=3);
+
+    return 0;
 }
